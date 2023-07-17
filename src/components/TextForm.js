d4d5f3770a1fe8a7f5221511d3012517bce4bca8
priyansu-nayak <priyansu18.8@gmail.com>
2023-07-17 16:21:19 +0530

    Updated TextForm

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,3 +1,43 @@
+//Video #7
+/*
+import React,{ useState } from 'react'
+
+
+export default function TextForm(props) {
+    const [text,setText] = useState('');
+    
+    const handleOnChange =(event)=>{
+        console.log("handleOnChange clicked!");
+        setText(event.target.value);    
+    }
+    
+    const handleUpClick = ()=>{
+        console.log("UpperCase clicked!");
+        setText(text.toUpperCase());
+    }
+
+    const handleLoClick =()=>{
+        console.log("lowercase clicked!");
+        setText(text.toLowerCase());
+    }
+
+    return (
+        <>
+            <h1>{props.heading}</h1>
+            <div className="mb-3">
+                <textarea className="form-control" id="myBox" rows="8" placeholder='Enter text here' onChange={handleOnChange} value={text} ></textarea>
+            </div>
+            <button className="btn btn-primary" onClick={handleUpClick}>UpperCase</button>
+            <button className="btn btn-primary" onClick={handleLoClick}>LowerCase</button>
+        </>
+    );
+}
+
+
+*/
+
+// Video #8
+
 import React,{ useState } from 'react'
 
 
