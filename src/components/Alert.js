f5da1adc482cb05c152374e85eb4ef3f1e7d72fb
priyansu-nayak <priyansu18.8@gmail.com>
2023-08-09 11:26:10 +0530

    updated

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Alert(props) {
 
-    const capitalize=(word)=>{
+    const capitalize = (word) => {
         const lower = word.toLowerCase();
         console.log(lower.charAt(0));
         console.log(lower.slice(1));
@@ -10,10 +10,12 @@ function Alert(props) {
     }
 
     return (
+        <div className="" style={{ height: '50px' }} >
+            {props.alert && <div class={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
+                <strong>{capitalize(props.alert.type)}</strong>:{props.alert.msg}
 
-        props.alert && <div class={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
-            <strong>{capitalize(props.alert.type)}</strong>:{props.alert.msg}
-     
+            </div>}
+
         </div>
 
 
